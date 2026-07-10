4d92b88bd9f640f901da19ab749d1a8fa57f6996
trantonminhky <fungusvalentinealt69420@gmail.com>
2026-07-10 08:30:58 +0700

    added UserRole table and learned the hard way schema doesn't exist in mysql

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,7 @@
 CREATE DATABASE Sanguosuo;
 USE Sanguosuo;
 
-CREATE SCHEMA lookup_table;
-
-CREATE TABLE lookup_table.UserStatus (
+CREATE TABLE UserStatus (
 	user_status_id TINYINT AUTO_INCREMENT,
 	user_status_code VARCHAR(20),
 	user_status_name VARCHAR(50) CHARACTER SET UTF8MB4,
@@ -14,4 +12,17 @@ CREATE TABLE lookup_table.UserStatus (
 		UNIQUE (user_status_code),
     CONSTRAINT CHK_UserStatus_user_status_code_uppercase
         CHECK (CAST(user_status_code AS BINARY) = UPPER(user_status_code))
+);
+
+CREATE TABLE UserRole (
+	user_role_id TINYINT AUTO_INCREMENT,
+	user_role_code VARCHAR(20),
+	user_role_name VARCHAR(50) CHARACTER SET UTF8MB4,
+
+	CONSTRAINT PK_UserRole_user_role_id
+		PRIMARY KEY (user_role_id),
+	CONSTRAINT UK_UserRole_user_role_code
+		UNIQUE (user_role_code),
+    CONSTRAINT CHK_UserRole_user_role_code_uppercase
+        CHECK (CAST(user_role_code AS BINARY) = UPPER(user_role_code))
 );
--- db/test.sql
+++
@@ -1,5 +0,0 @@
-USE Test;
-INSERT INTO TestTable VALUES (3, 'hello');
-INSERT INTO TestTable VALUES (4, 'world');
-
-SELECT * FROM TestTable;
