ec1021969ae527d3a5065cc9656fe5c0ce0bee73
trantonminhky <fungusvalentinealt69420@gmail.com>
2026-07-12 17:55:27 +0700

    seeded SkillTag

--- db/seed.sql
+++ db/seed.sql
@@ -9,3 +9,11 @@ INSERT INTO UserRole (user_role_code, user_role_name) VALUES
 	('VIEWER', 'Viewer'),
 	('EDITOR', 'Editor'),
 	('ADMIN', 'Administrator');
+
+INSERT INTO SkillTag (skill_tag_code, skill_tag_name) VALUES
+	('CTK', 'Chủ Tướng Kỹ'),
+	('PTK', 'Phó Tướng Kỹ'),
+	('TDK', 'Toả Định Kỹ'),
+	('HDK', 'Hạn Định Kỹ'),
+	('TPK', 'Trận Pháp Kỹ'),
+	('GM', 'Giảm 0.5 Máu');
