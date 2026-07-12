528d3d18f611dca70c17d809823d8c05ea588122
trantonminhky <fungusvalentinealt69420@gmail.com>
2026-07-12 17:27:40 +0700

    seeded UserRole

--- db/seed.sql
+++ db/seed.sql
@@ -4,3 +4,8 @@ INSERT INTO UserStatus (user_status_code, user_status_name) VALUES
 	('ACTIVE', 'Active'),
 	('INACTIVE', 'Inactive'),
 	('DISABLED', 'Disabled');
+
+INSERT INTO UserRole (user_role_code, user_role_name) VALUES
+	('VIEWER', 'Viewer'),
+	('EDITOR', 'Editor'),
+	('ADMIN', 'Administrator');
