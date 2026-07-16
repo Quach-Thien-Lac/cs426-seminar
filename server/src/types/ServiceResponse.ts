9b9430cfd58ec31a2efee3d067fed9c63bcb0862
trantonminhky <fungusvalentinealt69420@gmail.com>
2026-07-16 17:11:46 +0700

    added a check when i forget certain response fields

--- server/src/types/ServiceResponse.ts
+++ server/src/types/ServiceResponse.ts
@@ -14,9 +14,17 @@ interface IServiceResponse {
 class ServiceResponse implements IServiceResponse {
 	success: boolean = false;
 	statusCode: number = -1;
-	payload: ServiceResponsePayload = { message: "Default service response." }
+	payload: ServiceResponsePayload = { message: "miku" }
 
 	get() {
+		if (this.statusCode === -1) {
+			throw new Error("Status code is still in default value you dumbfuck");
+		}
+
+		if (this.payload.message = "miku") {
+			throw new Error("Payload message is still in default value you dumbfuck");
+		}
+
 		const response: IServiceResponse = {
 			success: this.success,
 			statusCode: this.statusCode,
