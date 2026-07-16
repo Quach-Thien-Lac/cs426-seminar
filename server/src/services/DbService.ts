50ffe3a2d5f443087ec6183808a8597ef7e563b3
trantonminhky <fungusvalentinealt69420@gmail.com>
2026-07-16 17:35:26 +0700

    added documentation for get-hero endpoint

--- server/src/services/DbService.ts
+++ server/src/services/DbService.ts
@@ -2,6 +2,19 @@ import DbConnection from "../connections/DbConnection.ts";
 import ServiceResponse from "../types/ServiceResponse.ts";
 
 class DbService {
+	/**
+	 * Service function for <code>/api/db/get-hero</code>. Get all data for a hero. Supports <code>QUERY</code> requests.
+	 * @param {string} hero - The hero's ID
+	 * @returns {Promise<ServiceResponse>}
+	 * 
+	 * @example <caption>cURL</caption>
+	 * curl -X POST \
+	 * --header 'Content-Type:application/json' \
+	 * --data '{"hero": "WEI015"}' \
+	 * http://localhost:8080/api/db/get-hero
+	 * 
+	 * @property {OK} 200 - Successful request
+	 */
 	async getHero(hero: string): Promise<ServiceResponse> {
 		const [results] = await DbConnection.pool.execute(`SELECT * FROM Hero WHERE hero_id = ?;`, [hero]);
 
