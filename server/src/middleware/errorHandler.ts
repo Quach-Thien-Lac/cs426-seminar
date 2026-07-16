25c2311aa86a9795269ac7bb2cc6d665e4bbe76c
trantonminhky <fungusvalentinealt69420@gmail.com>
2026-07-16 18:02:56 +0700

    added an error handler middleware

---
+++ server/src/middleware/errorHandler.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from 'express';
+import ServiceResponse from '../types/ServiceResponse.ts';
+
+function errorHandler(err: Error, req: Request, res: Response, next: NextFunction): void {
+	console.error(err);
+
+	const response = new ServiceResponse;
+	response.success = false;
+	response.statusCode = 500;
+	response.payload = {
+		message: "We are so cooked",
+		data: err.toString()
+	};
+
+	return void res.status(response.statusCode).json(response.get());
+}
