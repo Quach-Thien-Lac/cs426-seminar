674f919f5e8099b96b4364e38e31e2561f5bf72d
trantonminhky <fungusvalentinealt69420@gmail.com>
2026-07-17 09:56:25 +0700

    implemented a function to generate unique user ID

--- server/src/services/AuthService.ts
+++ server/src/services/AuthService.ts
@@ -1,3 +1,26 @@
+import { RowDataPacket, type ResultSetHeader } from 'mysql2';
+import type { UserRegistrationData } from "../types/UserRegistrationData.ts";
+import DbConnection from '../connections/DbConnection.ts';
+import randomstring from 'randomstring'
+
+async function generateUniqueUserID(): Promise<string> {
+	let userIDAlreadyExists: boolean = false;
+	let userID: string;
+
+	do {
+		userIDAlreadyExists = false;
+		userID = randomstring.generate({
+			length = 8,
+			charset: 'numeric'
+		});
+
+		const [results] = await DbConnection.pool.query<RowDataPacket[]>(`SELECT user_id FROM User WHERE user_id = ?`, [userID]);
+		userIDAlreadyExists = !!results.length;
+	} while (userID.startsWith('0') || userIDAlreadyExists);
+
+	return userID;
+}
+
 export class AuthService {
 	/**
 	 * @hidden
@@ -5,7 +28,15 @@ export class AuthService {
 	constructor() {
 	}
 
-	// public async register()
+	public async register(data: UserRegistrationData) {
+		let results: ResultSetHeader[];
+		let userID: string = await generateUniqueUserID();
+
+		try {
+
+			[results] = await DbConnection.pool.execute<ResultSetHeader[]>(``)
+		}
+	}
 }
 
 const authService = new AuthService();
