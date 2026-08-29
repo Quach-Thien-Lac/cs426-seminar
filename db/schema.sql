711785a4e0580ffb4c398c01436689d94d4c1a9b
trantonminhky <fungusvalentinealt69420@gmail.com>
2026-08-29 09:57:27 +0700

    removed hero_faction_id from Hero

--- db/schema.sql
+++ db/schema.sql
@@ -170,7 +170,6 @@ CREATE TABLE Hero (
 	hero_id VARCHAR(10),
 	hero_name VARCHAR(80) CHARACTER SET UTF8MB4,
 	hero_image_id VARCHAR(10),
-	hero_faction_id TINYINT NOT NULL,
 	hero_hp FLOAT(1) NOT NULL,
 	hero_epithet VARCHAR(100) CHARACTER SET UTF8MB4,
 	hero_quote VARCHAR(200) CHARACTER SET UTF8MB4,
